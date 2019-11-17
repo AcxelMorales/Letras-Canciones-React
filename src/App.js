7b4c6af40ff9d4b06ae5f6a577ce92b630e351a7
Acxel Morales <acxelmorales97@gmail.com>
2019-11-17 00:28:07 -0600

    Letters

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, Fragment } from 'react';
+import axios from 'axios';
+
+import Formulario from './components/Formulario';
+import Cancion from './components/Cancion';
+import Informacion from './components/Informacion';
 
 function App() {
+  const [artista, setArtista] = useState('');
+  const [letra, setLetra] = useState([]);
+  const [info, setInfo] = useState({});
+
+  const consultarAPILetra = async busqueda => {
+    const url = `https://api.lyrics.ovh/v1/${busqueda.artista}/${busqueda.cancion}`;
+    const respuesta = await axios.get(url);
+
+    setArtista(busqueda.artista);
+    setLetra(respuesta.data.lyrics);
+  };
+
+  const consultarAPIInfo = async () => {
+    if (artista) {
+      const url = `https://theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
+      const respuesta = await axios.get(url);
+
+      setInfo(respuesta.data.artists[0]);
+    }
+  };
+
+  useEffect(() => {
+    consultarAPIInfo();
+  }, [artista]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Formulario
+        consultarAPILetra={consultarAPILetra}
+      />
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+            <Informacion
+              info={info}
+            />
+          </div>
+          <div className="col-md-6">
+            <Cancion
+              letra={letra}
+            />
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
